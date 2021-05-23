f7666a34ca1d9ddced8498ec84407ebe1f1d9b67
linhdam961602 <linh.dam1602@gmail.com>
2021-05-23 22:11:42 +0700

    create video list

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,27 +1,89 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  ActivityIndicator,
+} from 'react-native';
 import { WebView } from 'react-native-webview';
 import { useTheme } from 'react-native-paper';
+
+import { videoList } from './mockData';
 
 import StyleCommon from 'theme/StyleCommon';
 
 function Home() {
   const { colors } = useTheme();
+  const [loading, setLoading] = useState(false);
+
+  const handleLoading = () => {
+    setLoading(!loading);
+  };
 
   return (
     <View
       style={[
         StyleCommon.container,
-        StyleCommon.centerContent,
+        styles.homeScreen,
         { backgroundColor: colors.background, color: colors.text },
       ]}>
-      <Text>Homepage</Text>
-      <WebView
-        source={{ uri: 'https://infinite.red' }}
-        style={{ marginTop: 20 }}
+      <View style={styles.videoIntro}>
+        <WebView
+          startInLoadingState
+          allowsFullscreenVideo={false}
+          mediaPlaybackRequiresUserAction={true}
+          onShouldStartLoadWithRequest={() => true}
+          startInLoadingState={true}
+          source={{
+            uri: 'https://www.youtube.com/watch?v=gb0fhDfMAxw&list=RDMM&start_radio=1&rv=fbmvNG13M7Y',
+          }}
+        />
+      </View>
+      <FlatList
+        data={videoList}
+        renderItem={({ item }) => (
+          <View style={styles.wrapperVideo}>
+            <View style={styles.video}>
+              <WebView
+                startInLoadingState
+                source={{ uri: item.uri }}
+                mediaPlaybackRequiresUserAction={true}
+                onShouldStartLoadWithRequest={() => true}
+                startInLoadingState={true}
+              />
+            </View>
+            <View style={{ flex: 1 }}>
+              <Text style={styles.videoTitle}>{item.title}</Text>
+            </View>
+          </View>
+        )}
+        keyExtractor={item => item.id}
       />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  homeScreen: {
+    paddingVertical: 15,
+  },
+  video: {
+    width: 150,
+    height: 100,
+    marginBottom: 15,
+  },
+  videoIntro: {
+    height: 150,
+    marginBottom: 20,
+  },
+  wrapperVideo: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  videoTitle: {
+    marginLeft: 10,
+  },
+});
 
 export default Home;
---
+++ src/screens/Home/mockData.js
@@ -0,0 +1,32 @@
+export const videoList = [
+  {
+    id: 1,
+    uri: 'https://www.youtube.com/watch?v=o9n-ilA9xOY',
+    title:
+      'Kiem tra chu co bi cat hay khong. Day la mot doan test kiem tra chu bi cat hay khong',
+  },
+  {
+    id: 2,
+    uri: 'https://www.youtube.com/watch?v=sdE6ngjEg_o',
+    title:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quiiverra purus. Aenean a urna vitae odio viverra porttitor vitae quis',
+  },
+  {
+    id: 3,
+    uri: 'https://www.youtube.com/watch?v=0dLr9W5BDCA',
+    title:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis',
+  },
+  {
+    id: 4,
+    uri: 'https://www.youtube.com/watch?v=0dLr9W5BDCA',
+    title:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis',
+  },
+  {
+    id: 5,
+    uri: 'https://www.youtube.com/watch?v=0dLr9W5BDCA',
+    title:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis',
+  },
+];
